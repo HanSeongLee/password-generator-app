5d99dbf07b8186b318fbe35447f3a6c278413003
HanSeongLee <jkhanseong@gmail.com>
2023-01-11 19:39:50 +0900

    ADD | Main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import type { NextPage } from 'next'
+import styles from './style.module.scss';
+import Container from 'components/Container';
+import PasswordGeneratorContainer from 'containers/PasswordGeneratorContainer';
 
 const Home: NextPage = () => {
-  return (
-      <>
-        <main>
-            <h1>Password Generator</h1>
-        </main>
-      </>
-  );
+    return (
+        <>
+            <main className={styles.main}>
+                <Container>
+                    <h1 className={styles.title}>
+                        Password Generator
+                    </h1>
+                    <PasswordGeneratorContainer className={styles.passwordGeneratorContainer} />
+                </Container>
+            </main>
+        </>
+    );
 }
 
 export default Home
